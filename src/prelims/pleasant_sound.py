df864d4e9053519a265b4dc0a66440f56527807e
chiggum <dhruvkohli99@gmail.com>
2017-10-08 01:14:42 +0530

    tested modified pysynth

--- src/prelims/pleasant_sound.py
+++ src/prelims/pleasant_sound.py
@@ -1,11 +1,33 @@
 import pysynth
 
 song = (
-    ('c', 4),
-    ('c*', 4), ('e', 4), ('g', 4),
-    ('g*', 2), ('g5', 4),
-    ('g5*', 4), ('r', 4), ('e5', 4),
-    ('e5*', 4) 
+    ('c', 8),
+    ('c*', 8), ('e', 8), ('g', 8),
+    ('g*', 2), ('g5', 8),
+    ('g5*', 8), ('r', 8), ('e5', 8),
+    ('e5*', 8) 
 )
 
-pysynth.make_wav(song, fn = "song.wav")
+traverse = [
+    ('c', 8), ('c#', 8), ('d', 8), ('d#', 8), ('e', 8), ('f', 8), ('f#', 8), ('g', 8), ('g#', 8),
+    ('a', 8), ('a#', 8), ('b', 8), 
+    ('c', 8), ('c#', 8), ('d', 8), ('d#', 8), ('e', 8), ('f', 8), ('f#', 8), ('g', 8), ('g#', 8),
+    ('a', 8), ('a#', 8), ('b', 8),
+    ('c', 8), ('c#', 8), ('d', 8), ('d#', 8), ('e', 8), ('f', 8), ('f#', 8), ('g', 8), ('g#', 8),
+    ('a', 8), ('a#', 8), ('b', 8),
+]
+
+traverse_unit = [
+    ('c', 8), ('c#', 8), ('d', 8), ('d#', 8), ('e', 8), ('f', 8), ('f#', 8), ('g', 8), ('g#', 8),
+    ('a', 8), ('a#', 8), ('b', 8)
+]
+
+# pysynth.make_wav(traverse, fn = "traverse.wav")
+
+f_ = None
+N=10
+for i in range(N):
+    if i == N-1:
+        f_ = pysynth.make_wav(traverse_unit, fn = "traverse_overlap.wav", superimpose=True, close_f = True, f=f_)
+    else:
+        f_ = pysynth.make_wav(traverse_unit, fn = "traverse_overlap.wav", superimpose=True, close_f = False, f=f_)
